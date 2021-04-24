00361d213ece6169c87733bf600caa2d546876fe
Cyzus Chi <chieftain@berkeley.edu>
2021-04-25 05:40:20 +0800

    Create AI.cs

---
+++ Project48/Assets/Scripts/Enemy/AI.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Jail
+{
+
+
+    public enum EnemyState
+    {
+        attack,
+        death,
+        stunned,
+        steady,
+        move
+    }
+
+
+    public enum AttackEffects
+    {
+        stun,
+        noEff
+    }
+    public class AI : MonoBehaviour
+    {
+
+        private Vector3 pos;
+        private GameObject player;
+        public EnemyState state = EnemyState.steady;
+        private float distance;
+        private float EncounterDis = 10f;
+        private float attackDis = 2f;
+
+        private int stunnedCounter = 0;
+        private NavMeshAgent agent; // not sure what type is that
+        public bool isAttacked = false;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+            pos = GetComponent<transform>();
+            player = GameObject.FindWithTag("Player");
+            agent = GetComponent<NavMeshAgent>();
+
+        }
+
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            AttackEffects attackEffect = AttackEffects.noEff;
+            if (isAttacked && attackEffect == AttackEffects.stun)
+            {
+                state = EnemyState.stunned;
+                stunnedCounter = 100;
+            }
+            else if (state == EnemyState.stunned)
+            {
+                if (stunnedCounter == 0)
+                {
+                    state = EnemyState.steady;
+                }
+                else
+                {
+                    stunnedCounter -= 1;
+                }
+                return;
+
+            }
+
+
+            distance = Vector3.Distance(player.position, pos.position);
+            if (EncounterDis >= distance )
+            {
+                state = EnemyState.steady;
+            } 
+            else if (distance < EncounterDis && attackDis <= distance)
+            {
+                state = EnemyState.move;
+                agent.SetDestination(player.transform.position);
+
+            } else {
+                state = EnemyState.attack;
+            }
+
+        }
+    }
+}
+
