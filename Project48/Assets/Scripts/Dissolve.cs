94e0df1e133e94253aaa19edfc4991ddd12c2194
Xiao <chairmantoad@gmail.com>
2021-06-17 19:34:21 +0800

    Add documentation for Dissolve.cs

--- Project48/Assets/Scripts/Dissolve.cs
+++ Project48/Assets/Scripts/Dissolve.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Dissolve script controls the dissolve shader
+/// </summary>
 [RequireComponent(typeof(Renderer))]
 public class Dissolve : MonoBehaviour
 {
