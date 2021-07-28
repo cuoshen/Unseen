09ca1516c1f763da549f371809b85478a5411358
SineNomine2001 <31798972+SineNomine2001@users.noreply.github.com>
2021-07-28 23:01:39 +0800

    Rename

--- Project48/Assets/Door.cs
+++ Project48/Assets/Door.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LockDoorRotation : MonoBehaviour
+public class Door : MonoBehaviour
 {
     void LateUpdate()
     {
