b7ea263c459da5a01cc290edca685a9925aa69be
linzexin <linzexinmasterchief@gmail.com>
2021-08-05 01:43:41 +0800

    add platform

---
+++ Project48/Assets/Scripts/Environment/PlatformCarry.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformCarry : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.transform.tag == "Player")
+        {
+            collision.collider.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        collision.collider.transform.SetParent(null);
+    }
+}
